01a96f186a19a82761959259dbd729c3e7d92add
Jonathan Karsh <jonathan.karsh@nearmap.com>
2015-09-13 00:21:57 +1000

    CertIterate iterates X509 certificates

---
+++ CertIterate/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
+
+namespace CertIterate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (var storelocation in (StoreLocation[])Enum.GetValues(typeof(StoreLocation)))
+            {
+                foreach (var storename in (StoreName[])Enum.GetValues(typeof(StoreName)))
+                {
+                    var store = new X509Store(storename, storelocation);
+                    try
+                    {
+                        store.Open(OpenFlags.OpenExistingOnly);
+                    }
+                    catch (CryptographicException)
+                    {
+                        // ignore
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine("Failed enumeration store: {0}, {1}: {2}", storename, storelocation, e);
+                    }
+                    try
+                    {
+                        Console.WriteLine("Found {0} certificates in store: {1}, {2}", store.Certificates.Count, storename, storelocation);
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine("Failed printing store details: {0}, {1}: {2}", storename, storelocation, e);
+                    }
+                }
+            }
+        }
+    }
+}
